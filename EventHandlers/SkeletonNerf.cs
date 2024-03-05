2d2400dd8f572bc1e935f3fe0761bff636eb0205
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-05 12:06:02 -0600

    Create SkeletonNerf.cs

---
+++ EventHandlers/SkeletonNerf.cs
@@ -0,0 +1,12 @@
+using Exiled.API.Features;
+
+namespace AnomalousZonePlugin.EventHandlers
+{
+  internal sealed class SkeletonNerf
+  {
+    private Plugin plugin;
+    public SkeletonNerf (Plugin plugin) => this.plugin = plugin;
+    {
+    }
+  }
+}
