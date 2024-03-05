53c1200eab87d2d9a164bb392280fb87881cc3ec
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-05 12:12:30 -0600

    Update NNN.cs

--- EventHandlers/NNN.cs
+++ EventHandlers/NNN.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace AnomalousZonePlugin.EventHandlers
 {
-    internal class NNN
+    internal sealed class NNN
     {
+        private Plugin plugin;
+        public NNN(Plugin plugin) => this.plugin = plugin;
+        private List<RoleTypeId> ScpRoles = new List<RoleTypeId> 
+        {
+            RoleTypeId.Scp049,
+            RoleTypeId.Scp096,
+            RoleTypeId.Scp106,
+            RoleTypeId.Scp939,
+            RoleTypeId.Scp3114,
+        };
+        private RoleTypeId GetAvailableScp()
+        {
+            RoleTypeId chosen = RoleTypeId.None;
+            ScpRoles.ShuffleList();
+            foreach (RoleTypeId role in ScpRoles)
+            {
+                if (Player.Get(role).Count() < 1)
+                {
+                    chosen = role;
+                    break;
+                }
+            }
+            if (chosen == RoleTypeId.None)
+                chosen = ScpRoles.First();
+            return chosen;
+        }
+        public void OnChangingRole(ChangingRoleEventArgs ev)
+        {
+            if (ev.NewRole == RoleTypeId.Scp173 && DateTime.Now.Month == 11)
+            {
+                if (ev.Reason == SpawnReason.ForceClass)
+                {
+                    return;
+                }
+                ev.NewRole = GetAvailableScp();
+            }
+        }
     }
 }
