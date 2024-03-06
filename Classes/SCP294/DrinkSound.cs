69ef9abad87a24591fdc21543663e6170b6e6048
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-06 15:03:08 -0600

    Update DrinkSound.cs

---
+++ Classes/SCP294/DrinkSound.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnomalousZonePlugin.Classes.SCP294
+{
+    public enum DrinkSound
+    {
+        Normal = 0,
+        Unstable = 1
+    }
+    public class DrinkSoundFiles
+    {
+        public List<string> List = new List<string>() {
+            "slurp.ogg",
+            "spit.ogg",
+            "Vomit.ogg",
+            "ahh.ogg",
+            "burn.ogg",
+            "coin_drop.ogg",
+            "cough.ogg",
+            "dispense0.ogg",
+            "dispense1.ogg",
+            "dispense2.ogg",
+            "dispense3.ogg",
+            "ew1.ogg",
+            "ew2.ogg",
+            "outofrange.ogg",
+            "retch1.ogg",
+            "retch2.ogg"
+        };
+    }
+}
