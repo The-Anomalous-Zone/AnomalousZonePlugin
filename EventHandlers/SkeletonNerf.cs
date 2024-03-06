1e5f10f15a8a5199b66e553ca7f8be496bff48b7
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-06 11:39:34 -0600

    Update SkeletonNerf.cs

--- EventHandlers/SkeletonNerf.cs
+++ EventHandlers/SkeletonNerf.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Exiled.API.Features;
+using Exiled.API.Enums;
+using PlayerRoles;
+using Exiled.Events.EventArgs.Player;
 
 namespace AnomalousZonePlugin.EventHandlers
 {
-    internal class SkeletonNerf
+    internal sealed class SkeletonNerf
     {
+        private Plugin plugin;
+        public SkeletonNerf(Plugin plugin) => this.plugin = plugin;
+        public OnHurting(HurtingEventArgs ev)
+        {
+            if (!ev.Attacker == RoleTypeId.Scp3114)
+                return;
+            if (ev.Damage == DamageType.Strangled)
+            {
+                Timing.CallDelayed(Plugin.Instance.Config.StrangleLimit, () =>
+                {
+                    ev.IsAllowed = false;
+                    Timing.CallDelayed(Plugin.Instance.Config.StrangleCooldown, () =>
+                    {
+                        ev.IsAllowed = true;
+                    });
+                });
+            }
+        }
     }
 }
