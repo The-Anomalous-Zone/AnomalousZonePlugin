b5c06df18dc51aed100f253e3cdc38488ceb212f
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-06 15:02:14 -0600

    Create DrinkInfo.cs

---
+++ Classes/SCP294/DrinkInfo.cs
@@ -0,0 +1,36 @@
+﻿using Exiled.API.Features;
+using Exiled.API.Features.Items;
+using InventorySystem.Items;
+using AnomalousZonePlugin.Configs.SCP294;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace AnomalousZonePlugin.Classes.SCP294
+{
+    public class DrinkInfo
+    {
+        public ushort ItemSerial { get; set; } = 0;     
+        public Item ItemObject { get; set; } = null;
+        public string DrinkName { get; set; } = "";
+        public string DrinkMessage { get; set; } = "";
+        public bool KillPlayer { get; set; } = false;
+        public string KillPlayerString { get; set; } = "";
+        public float HealAmount { get; set; } = 0;
+        public bool HealStatusEffects { get; set; } = false;
+        public bool Tantrum { get; set; } = false;
+        public List<DrinkEffect> DrinkEffects { get; set; } = new List<DrinkEffect>()
+        {
+
+        };
+        public Action<Player> DrinkCallback = null;
+
+        public static bool IsCustomDrink(ItemBase itembase)
+        {
+            return SCP294.Instance.CustomDrinkItems.Keys.Contains(itembase.ItemSerial);
+        }
+    }
+}
