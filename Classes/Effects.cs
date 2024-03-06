c0b88206fffb9f0bb27ae1c5d00f04193e19baf1
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-06 11:12:30 -0600

    Update Effects.cs

--- Classes/Effects.cs
+++ Classes/Effects.cs
@@ -7,21 +7,33 @@ namespace AnomalousZonePlugin.Classes
 {
     public class Effects
     {
-      private StatusEffectBase Effect;
-      private byte Intensity;
-      private byte CurrentIntensity;
-      private float Duration;
+        private StatusEffectBase Effect;
+        private byte Intensity;
+        private byte CurrentIntensity;
+        private float Duration;
 
-      public Effects(StatusEffectBase Effect, byte Intensity, byte CurrentIntensity, float Duration)
-      {
-        this.Effect = Effect;
-        this.Intensity = Intensity;
-        this.CurrentIntensity = CurrentIntensity;
-        this.Duration = Duration;
-      }
-     public StatusEffectBase GetEffect()
-     {
-         return Effect;
-     }
+        public Effects(StatusEffectBase Effect, byte Intensity, byte CurrentIntensity, float Duration)
+        {
+            this.Effect = Effect;
+            this.Intensity = Intensity;
+            this.CurrentIntensity = CurrentIntensity;
+            this.Duration = Duration;
+        }
+        public StatusEffectBase GetEffect()
+        {
+            return Effect;
+        }
+        public byte GetIntensity()
+        {
+            return Intensity;
+        }
+        public byte GetCurrentIntensity()
+        {
+            return CurrentIntensity;
+        }
+        public float GetDuration()
+        {
+            return Duration;
+        }
     }
 }
