81f10f0b1a5f94047fd04f9231b8ef544ea65417
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2024-03-06 13:51:44 -0600

    Update Disguise.cs

--- Classes/FunnyPills/Disguise.cs
+++ Classes/FunnyPills/Disguise.cs
@@ -59,6 +59,13 @@ namespace AnomalousZonePlugin.Classes.FunnyPills
                     }
                 break;
             }
+            player.ShowHint($"<b><color=#0d98ba>You've been disguised as a </color><color={disguise.GetColor().ToHex()}>{disguise.GetFullName()}</color><color=#0d98ba> temporarily!");
+            player.ChangeAppearance(disguise);
+            Timing.CallDelayed(UnityEngine.Random.Range(Plugin.Instance.Config.PillsDisguiseMinTime, (Plugin.Instance.Config.PillsDisguiseMaxTime * 60)), () =>
+            {
+                player.ShowHint($"<color=#0d98ba>Your disugise has worn off!</color>");
+                player.ChangeAppearance(player.Role.Type);
+            });
         }
     }
 }
