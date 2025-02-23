f373b8415029bfc9764922d8211943ef1a2bc2a1
Logan Dresel <119338946+coollogan876@users.noreply.github.com>
2025-02-23 10:12:35 -0600

    I didn't

--- Classes/SCPSuicide/Warn.cs
+++ Classes/SCPSuicide/Warn.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace AnomalousZonePlugin.Classes.SCPSuicide
 {
+    // I didn't like the SCP suicide plugin I added so
+    // I started working on my own
     internal class Warn
     {
     }
