6aaa87364fe322816f449f8a2e4b840728e76484
Logan Dresel <coollogan876@gmail.com>
2025-02-24 23:32:10 -0600

    Added inverted controls

--- Classes/FunnyPills/InvertedControls.cs
+++ Classes/FunnyPills/InvertedControls.cs
@@ -1,18 +1,26 @@
 ﻿using Exiled.API.Features;
+using MEC;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Windows;
 
 namespace AnomalousZonePlugin.Classes.FunnyPills
 {
-    public class InvertedControls
+    internal sealed class InvertedControls
     {
         public static void Invert(Player player)
         {
-            // I forgor
-        }
+            // Apparently if you slow down a player enough, their controls just invert
+            player.EnableEffect(Exiled.API.Enums.EffectType.Slowness, 200, 0, false);
+            Timing.CallDelayed(15, () =>
+            {
+                player.DisableEffect(Exiled.API.Enums.EffectType.Slowness);
+            });
+        }        
     }
 }
