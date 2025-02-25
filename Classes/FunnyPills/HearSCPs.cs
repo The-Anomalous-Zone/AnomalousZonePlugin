f47b491d655f54da40290e06051225203f369f0b
Logan Dresel <coollogan876@gmail.com>
2025-02-24 23:30:58 -0600

    Fix voicechat stuff

--- Classes/FunnyPills/HearSCPs.cs
+++ Classes/FunnyPills/HearSCPs.cs
@@ -7,47 +7,32 @@ using System.Threading.Tasks;
 using PlayerRoles;
 using UnityEngine;
 using MEC;
+using VoiceChat.Networking;
+using VoiceChat;
 
 namespace AnomalousZonePlugin.Classes.FunnyPills
 {
     // This stopped working
     public class HearSCPs
     {
-        public bool Hear;
-        private Player player;
-        private static Dictionary<Player, HearSCPs> players = new Dictionary<Player, HearSCPs>();
-        public HearSCPs(Player player)
-        {
-            this.player = player;
-            players.Add(player, this);
-        }
-        public void Talk()
+        public static void hearSCPs(Player player)
         {
             var SCPs = Player.List.Where(scp => scp.IsScp).ToList();
             var scp = SCPs.Random();
             player.ShowHint($"<color=#0d98ba>You can talk to your favorite </color><color=#F00>{Plugin.Instance.Config.SCPNicknames[scp.Role.Type].Random()}</color<color=#0d98ba>!</color>");
 
-            Hear = true;
-            player.VoiceChannel = VoiceChat.VoiceChatChannel.ScpChat;
-            Timing.CallDelayed(UnityEngine.Random.Range(Plugin.Instance.Config.SCPChatMinTime, Plugin.Instance.Config.SCPChatMaxTime), ()=>
+            Plugin.Instance.channels.Add(player, VoiceChat.VoiceChatChannel.ScpChat);
+            Timing.CallDelayed(UnityEngine.Random.Range(Plugin.Instance.Config.SCPChatMinTime, Plugin.Instance.Config.SCPChatMaxTime), () =>
             {
-                Hear = false;
+                Plugin.Instance.channels.Remove(player);
                 if (!player.IsAlive)
                 {
-                    player.VoiceChannel = VoiceChat.VoiceChatChannel.Spectator;
                     return;
                 }
-                if (player.VoiceChannel == VoiceChat.VoiceChatChannel.ScpChat)
-                {
-                    player.ShowHint($"The SCPs didn't enjoy talking to you and are coming to murder you");
-                    player.VoiceChannel = VoiceChat.VoiceChatChannel.Proximity;
-                }
+                player.ShowHint($"The SCPs didn't enjoy talking to you and are coming to murder you");
+                                 // Good 👍
             });
-        }
 
-        public static HearSCPs Get(Player player)
-        {
-            return players[player];
-        }
+        }       
     }
 }
---
+++ Classes/FunnyPills/ModdedVoiceChat.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using Mirror;
+using PlayerRoles.Spectating;
+using PlayerRoles;
+using PluginAPI.Core.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using VoiceChat.Networking;
+using NorthwoodLib.Pools;
+using Exiled.Events.EventArgs.Player;
+using PlayerRoles.Voice;
+
+namespace AnomalousZonePlugin.Classes.FunnyPills
+
+{    
+    internal sealed class ModdedVoiceChat
+    {
+        private Plugin plugin;
+        public ModdedVoiceChat(Plugin plugin) => this.plugin = plugin;
+        public void OnPlayerUsingVoiceChat(VoiceChattingEventArgs ev)
+        {
+            if (!Plugin.Instance.channels.ContainsKey(ev.Player))
+            {
+                return;
+            }
+            ev.IsAllowed = false; // Prevent default behavior from causing weird stuff
+
+            VoiceMessage message = ev.VoiceMessage;
+            message.Channel = Plugin.Instance.channels[ev.Player];
+
+            foreach (ReferenceHub referenceHub in ReferenceHub.AllHubs)
+            {
+                if (referenceHub.roleManager.CurrentRole is IVoiceRole voiceRole)
+                {
+                    voiceRole.VoiceModule.ValidateReceive(message.Speaker, message.Channel);
+                    referenceHub.connectionToClient.Send(message);
+                }
+            }
+        }
+    }
+
+}
